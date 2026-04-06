caa0414c4213b1bf46658d5a63b7a73a598da894
Josh Hollandsworth <142175+joshholl@users.noreply.github.com>
2026-04-06 12:06:05 -0400

    updating lsp to fix error with schemastore and to add better typescript handling

--- home/dot_config/nvim/lua/plugins/lsp.lua
+++ home/dot_config/nvim/lua/plugins/lsp.lua
@@ -1,230 +1,233 @@
--- Shared LSP on_attach (keymaps applied to every LSP buffer)
+-- 🔑 Shared on_attach (used by ALL LSPs including TS tools)
 local function on_attach(_, bufnr)
   local map = function(mode, lhs, rhs, desc)
-    vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, noremap = true, silent = true, desc = desc })
+    vim.keymap.set(mode, lhs, rhs, {
+      buffer = bufnr,
+      silent = true,
+      desc = desc,
+    })
   end
 
-  map("n", "gd", vim.lsp.buf.definition, "Go to definition")
-  map("n", "gD", vim.lsp.buf.declaration, "Go to declaration")
+  map("n", "gd", vim.lsp.buf.definition, "Definition")
+  map("n", "gD", vim.lsp.buf.declaration, "Declaration")
   map("n", "gr", vim.lsp.buf.references, "References")
-  map("n", "gi", vim.lsp.buf.implementation, "Go to implementation")
-  map("n", "gt", vim.lsp.buf.type_definition, "Type definition")
-  map("n", "K", vim.lsp.buf.hover, "Hover docs")
-  map("n", "<C-k>", vim.lsp.buf.signature_help, "Signature help")
+  map("n", "gi", vim.lsp.buf.implementation, "Implementation")
+  map("n", "K", vim.lsp.buf.hover, "Hover")
+  map("n", "<C-k>", vim.lsp.buf.signature_help, "Signature Help")
+
   map("n", "<leader>rn", vim.lsp.buf.rename, "Rename")
-  map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, "Code action")
-  map("n", "<leader>lf", function() vim.lsp.buf.format({ async = true }) end, "Format buffer")
-  map("n", "<leader>li", "<cmd>LspInfo<CR>", "LSP info")
-  map("n", "<leader>lr", "<cmd>LspRestart<CR>", "LSP restart")
-  map("n", "gl", vim.diagnostic.open_float, "Line diagnostics")
-  map("n", "]d", vim.diagnostic.goto_next, "Next diagnostic")
-  map("n", "[d", vim.diagnostic.goto_prev, "Prev diagnostic")
+  map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, "Code Action")
+
+  map("n", "gl", vim.diagnostic.open_float, "Line Diagnostics")
+  map("n", "[d", vim.diagnostic.goto_prev, "Prev Diagnostic")
+  map("n", "]d", vim.diagnostic.goto_next, "Next Diagnostic")
+
+  map("n", "<leader>lf", function()
+    vim.lsp.buf.format({ async = true })
+  end, "Format")
 end
-
--- Diagnostic config
-vim.diagnostic.config({
-  virtual_text = { prefix = "●" },
-  signs = true,
-  update_in_insert = false,
-  underline = true,
-  severity_sort = true,
-  float = { border = "rounded", source = "always" },
-})
-
--- Diagnostic signs
-local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-end
-
--- Rounded borders for hover/signature
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-  vim.lsp.handlers.hover, { border = "rounded" }
-)
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-  vim.lsp.handlers.signature_help, { border = "rounded" }
-)
-
--- Servers configured via lspconfig (non-Java, non-Scala)
-local servers = {
-  -- Python
-  pyright = {
-    settings = {
-      python = {
-        analysis = {
-          typeCheckingMode = "basic",
-          autoSearchPaths = true,
-          useLibraryCodeForTypes = true,
-        },
-      },
-    },
-  },
-
-  -- TypeScript / JavaScript
-  ts_ls = {
-    settings = {
-      typescript = {
-        inlayHints = {
-          includeInlayParameterNameHints = "all",
-          includeInlayFunctionParameterTypeHints = true,
-          includeInlayVariableTypeHints = true,
-          includeInlayPropertyDeclarationTypeHints = true,
-          includeInlayFunctionLikeReturnTypeHints = true,
-          includeInlayEnumMemberValueHints = true,
-        },
-      },
-      javascript = {
-        inlayHints = {
-          includeInlayParameterNameHints = "all",
-          includeInlayFunctionParameterTypeHints = true,
-        },
-      },
-    },
-  },
-
-  -- Lua (for editing this config)
-  lua_ls = {
-    settings = {
-      Lua = {
-        runtime = { version = "LuaJIT" },
-        workspace = {
-          checkThirdParty = false,
-          library = vim.api.nvim_get_runtime_file("", true),
-        },
-        diagnostics = { globals = { "vim" } },
-        telemetry = { enable = false },
-      },
-    },
-  },
-
-  -- JSON
-  jsonls = {
-    settings = {
-      json = {
-        schemas = require("schemastore").json.schemas(),
-        validate = { enable = true },
-      },
-    },
-  },
-
-  -- ESLint (TypeScript/JavaScript linting)
-  eslint = {},
-
-  -- YAML
-  yamlls = {},
-
-  -- Bash
-  bashls = {},
-
-  -- CSS
-  cssls = {},
-
-  -- HTML
-  html = {},
-}
 
 return {
-  -- Schema store for jsonls/yamlls
-  {
-    "b0o/SchemaStore.nvim",
-    lazy = true,
-    version = false,
-  },
-
-  -- Mason: LSP/linter/formatter installer
+  -- 🧠 Mason
   {
     "williamboman/mason.nvim",
     build = ":MasonUpdate",
-    cmd = "Mason",
     opts = {
-      ui = {
-        border = "rounded",
-        icons = { package_installed = "✓", package_pending = "➜", package_uninstalled = "✗" },
-      },
+      ui = { border = "rounded" },
     },
   },
 
-  -- Bridge between Mason and lspconfig
+  -- 🔗 Mason bridge
   {
     "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim" },
+    dependencies = "mason.nvim",
     opts = {
-      ensure_installed = vim.tbl_keys(servers),
       automatic_installation = true,
     },
   },
 
-  -- LSP config
+  -- 📦 Schema store
+  {
+    "b0o/SchemaStore.nvim",
+    lazy = true,
+  },
+
+  -- 🧠 Better Lua LSP
+  {
+    "folke/neodev.nvim",
+    ft = "lua",
+    opts = {},
+  },
+
+  -- 🔄 LSP progress UI
+  {
+    "j-hui/fidget.nvim",
+    tag = "legacy",
+    event = "LspAttach",
+    opts = {},
+  },
+
+  -- ⚡ LSP Config
   {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
-      "williamboman/mason-lspconfig.nvim",
+      "mason.nvim",
+      "mason-lspconfig.nvim",
       "hrsh7th/cmp-nvim-lsp",
       "b0o/SchemaStore.nvim",
+      "folke/neodev.nvim",
     },
+
     config = function()
       local lspconfig = require("lspconfig")
+
+      -- safe require
+      local ok_schemastore, schemastore = pcall(require, "schemastore")
+
       local capabilities = vim.tbl_deep_extend(
         "force",
         vim.lsp.protocol.make_client_capabilities(),
         require("cmp_nvim_lsp").default_capabilities()
       )
 
-      for server, config in pairs(servers) do
-        config.on_attach = on_attach
-        config.capabilities = capabilities
-        lspconfig[server].setup(config)
+      local servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagnostics = { globals = { "vim" } },
+              workspace = {
+                checkThirdParty = false,
+                library = vim.api.nvim_get_runtime_file("", true),
+              },
+              telemetry = { enable = false },
+            },
+          },
+        },
+
+        pyright = {},
+
+        jsonls = ok_schemastore and {
+          settings = {
+            json = {
+              schemas = schemastore.json.schemas(),
+              validate = { enable = true },
+            },
+          },
+        } or {},
+
+        yamlls = ok_schemastore and {
+          settings = {
+            yaml = {
+              schemas = schemastore.yaml.schemas(),
+            },
+          },
+        } or {},
+
+        bashls = {},
+        html = {},
+        cssls = {},
+        eslint = {},
+      }
+
+      for name, opts in pairs(servers) do
+        opts.on_attach = on_attach
+        opts.capabilities = capabilities
+        lspconfig[name].setup(opts)
       end
+
+      -- diagnostics UI
+      vim.diagnostic.config({
+        virtual_text = { prefix = "●" },
+        float = { border = "rounded", source = "always" },
+        underline = true,
+        severity_sort = true,
+      })
     end,
   },
 
-  -- Formatting (replaces null-ls, which is archived)
+  -- 🔥 TypeScript (replaces tsserver completely)
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    ft = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+
+    opts = function()
+      local capabilities = vim.tbl_deep_extend(
+        "force",
+        vim.lsp.protocol.make_client_capabilities(),
+        require("cmp_nvim_lsp").default_capabilities()
+      )
+
+      return {
+        on_attach = on_attach,
+        capabilities = capabilities,
+
+        settings = {
+          separate_diagnostic_server = true,
+          publish_diagnostic_on = "insert_leave",
+          expose_as_code_action = "all",
+
+          tsserver_file_preferences = {
+            includeInlayParameterNameHints = "all",
+            includeInlayFunctionParameterTypeHints = true,
+            includeInlayVariableTypeHints = true,
+            includeInlayPropertyDeclarationTypeHints = true,
+            includeInlayFunctionLikeReturnTypeHints = true,
+            includeInlayEnumMemberValueHints = true,
+          },
+        },
+      }
+    end,
+
+    config = function(_, opts)
+      require("typescript-tools").setup(opts)
+
+      -- 🔥 Useful keymaps
+      vim.keymap.set("n", "<leader>to", "<cmd>TSToolsOrganizeImports<CR>", { desc = "Organize Imports" })
+      vim.keymap.set("n", "<leader>tR", "<cmd>TSToolsRenameFile<CR>", { desc = "Rename File" })
+      vim.keymap.set("n", "<leader>ta", "<cmd>TSToolsAddMissingImports<CR>", { desc = "Add Missing Imports" })
+      vim.keymap.set("n", "<leader>tu", "<cmd>TSToolsRemoveUnused<CR>", { desc = "Remove Unused Imports" })
+    end,
+  },
+
+  -- 🎨 Formatter
   {
     "stevearc/conform.nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    keys = {
-      {
-        "<leader>cf",
-        function() require("conform").format({ async = true, lsp_fallback = true }) end,
-        desc = "Format buffer (conform)",
-      },
-    },
+    event = "BufWritePre",
     opts = {
       formatters_by_ft = {
         lua = { "stylua" },
         python = { "black", "isort" },
         javascript = { "prettier" },
-        javascriptreact = { "prettier" },
         typescript = { "prettier" },
-        typescriptreact = { "prettier" },
         json = { "prettier" },
         yaml = { "prettier" },
-        markdown = { "prettier" },
-        css = { "prettier" },
-        html = { "prettier" },
-        sh = { "shfmt" },
       },
-      format_on_save = { timeout_ms = 500, lsp_fallback = true },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
     },
   },
 
-  -- Linting
+  -- 🔍 Linting
   {
     "mfussenegger/nvim-lint",
-    event = { "BufReadPost", "BufNewFile" },
+    event = "BufReadPost",
     config = function()
       local lint = require("lint")
+
       lint.linters_by_ft = {
         python = { "ruff" },
         javascript = { "eslint_d" },
         typescript = { "eslint_d" },
-        javascriptreact = { "eslint_d" },
-        typescriptreact = { "eslint_d" },
       }
-      vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
+
+      vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave" }, {
         callback = function()
           lint.try_lint()
         end,
@@ -232,17 +235,18 @@ return {
     end,
   },
 
-  -- Mason tool installer (formatters/linters not in mason-lspconfig)
+  -- 🧰 Tools
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
-    dependencies = { "williamboman/mason.nvim" },
+    dependencies = "mason.nvim",
     opts = {
       ensure_installed = {
-        "black", "isort", "ruff",
-        "prettier", "eslint_d",
-        "stylua", "shfmt",
-        "jdtls",
-        "scala-language-server",
+        "stylua",
+        "black",
+        "isort",
+        "ruff",
+        "prettier",
+        "eslint_d",
       },
     },
   },
