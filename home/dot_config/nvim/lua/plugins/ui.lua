b42270ea70b8986db3a1e3eb8869316f68a2ee7c
Josh Hollandsworth <142175+joshholl@users.noreply.github.com>
2026-04-07 05:48:02 -0400

    fixing lualine issue with catpuccin theme

--- home/dot_config/nvim/lua/plugins/ui.lua
+++ home/dot_config/nvim/lua/plugins/ui.lua
@@ -38,22 +38,25 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
-    opts = {
-      options = {
-        theme = "catppuccin",
-        globalstatus = true,
-        component_separators = "|",
-        section_separators = { left = "", right = "" },
-      },
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = { { "filename", path = 1 } },
-        lualine_x = { "encoding", "fileformat", "filetype" },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
-      },
-    },
+    dependencies = { "catppuccin/nvim" },
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = require("catppuccin.utils.lualine"),
+          globalstatus = true,
+          component_separators = "|",
+          section_separators = { left = "", right = "" },
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch", "diff", "diagnostics" },
+          lualine_c = { { "filename", path = 1 } },
+          lualine_x = { "encoding", "fileformat", "filetype" },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
+        },
+      })
+    end,
   },
 
   -- Buffer tabs
